53438446a5b12414ff2035ccf96fb0ff05769081
damianlujep <Comecalzonn1>
2020-11-20 22:29:45 +0100

    HotelService and HotelSearchingDto

--- src/main/java/pl/bookingsystem/app/controller/MainPageController.java
+++ src/main/java/pl/bookingsystem/app/controller/MainPageController.java
@@ -1,15 +1,31 @@
 package pl.bookingsystem.app.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import pl.bookingsystem.app.dto.HotelSearchingDto;
+import pl.bookingsystem.app.entity.Hotel;
+import pl.bookingsystem.app.services.HotelService;
+
+import java.util.List;
 
 
 @Controller
 public class MainPageController {
+    private final HotelService hotelService;
+
+    @Autowired
+    public MainPageController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
 
     @GetMapping("/")
-    public String homePageController(){
-        return "/main-content/home";
+    public ModelAndView homePageController(){
+        ModelAndView homePage = new ModelAndView("/main-content/home");
+        homePage.addObject("hotelSearching", new HotelSearchingDto());
+        return homePage ;
     }
 
     @GetMapping("/login")
@@ -21,5 +37,10 @@ public class MainPageController {
     @GetMapping("/registration")
     public String showRegistrationForm(){
         return "/main-content/registration-form";
+    }
+
+    @ModelAttribute("allCitiesList")
+    public List<String> allCitiesList(){
+        return  hotelService.getAllHotelCities();
     }
 }
---
+++ src/main/java/pl/bookingsystem/app/services/IHotelServices.java
@@ -0,0 +1,7 @@
+package pl.bookingsystem.app.services;
+
+import java.util.List;
+
+public interface IHotelServices {
+    List<String> getAllHotelCities();
+}
